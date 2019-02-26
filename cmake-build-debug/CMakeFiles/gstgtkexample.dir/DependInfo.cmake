ca4d171b75841c87c37beb249ded637ae50a1556
Odisseas Nikolis <odynik@iti.gr>
2019-02-26 12:32:59 +0200

    CLion project with a basic tutorial 1 of gstreamer. RSTP IP camera pipeline rather than a simple video from web. To execute you need to install gstreamer and its dependencies.

---
+++ cmake-build-debug/CMakeFiles/gstgtkexample.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/odynik/CLionProjects/ipcamviewer/main.c" "C:/Users/odynik/CLionProjects/ipcamviewer/cmake-build-debug/CMakeFiles/gstgtkexample.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../gstgtkexample"
+  "C:/gstreamer-dev/1.0/x86_64/include/gstreamer-1.0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/gstgtkexample.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gstgtkexample.dir/main.c.obj"
+  "gstgtkexample.pdb"
+  "gstgtkexample.exe"
+  "gstgtkexample.exe.manifest"
+  "libgstgtkexample.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gstgtkexample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
